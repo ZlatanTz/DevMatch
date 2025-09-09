f3dd3ea8b45b5c689bcc63ca3dcd2e6e0a8a0772
Zlatan Tuzovic <tuzovic.zlatan@gmail.com>
2025-09-09 13:45:32 +0200

    chore: add eslint prettier setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2023: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: "detect" },
+    "import/resolver": {
+      node: { extensions: [".js", ".jsx"] },
+    },
+  },
+  plugins: ["react", "react-hooks", "import", "jsx-a11y", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended", // enables Prettier + shows formatting as ESLint errors
+  ],
+  rules: {
+    "prettier/prettier": ["error", { endOfLine: "auto" }],
+    "react/react-in-jsx-scope": "off", // not needed with React 17+
+    "react/prop-types": "off", // turn on if you use PropTypes
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "import/order": [
+      "warn",
+      {
+        "newlines-between": "always",
+        groups: ["builtin", "external", "internal", ["parent", "sibling", "index"]],
+        alphabetize: { order: "asc", caseInsensitive: true },
+      },
+    ],
+    "import/newline-after-import": "warn",
+    "import/no-unresolved": "off", // Vite handles aliases; enable if needed
+  },
+  ignorePatterns: ["dist/", "build/", "coverage/", "node_modules/", "*.config.*"],
+};
