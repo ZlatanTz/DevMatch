ff486ca0103d91054749d02bcf47a7168d7c500e
Zlatan Tuzovic <tuzovic.zlatan@gmail.com>
2025-09-09 14:37:02 +0200

    commit before pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-federal-blue font-bold">Tailwind v4 works</h1>
+      <h1 className="text-3xl --color-white font-bold">Tailwind v4 works</h1>
       <p className="text-base text-paynes-gray">Roboto + custom theme tokens ✔</p>
       <button className="bg-emerald text-white px-4 py-2 rounded">Apply</button>
       <h1>TEKST </h1>
