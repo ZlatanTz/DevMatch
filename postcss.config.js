613be04d389b547e8dc6caa28d224cb463ebc38c
Zlatan Tuzovic <tuzovic.zlatan@gmail.com>
2025-09-09 14:23:01 +0200

    chore: added tailwind and config

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+// postcss.config.js
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {}, // ✅ correct way for Tailwind v4
+  },
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-      <h1>TEKST</h1>
+      <h1 className="text-3xl text-federal-blue font-bold">Tailwind v4 works</h1>
+      <p className="text-base text-paynes-gray">Roboto + custom theme tokens ✔</p>
+      <button className="bg-emerald text-white px-4 py-2 rounded">Apply</button>
+      <h1>TEKST </h1>
+      <p></p>
     </>
   );
 }
