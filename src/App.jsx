fa0b30222e15458be7c27185396c10aa51d16386
eris00 <erissutkovic00@gmail.com>
2025-09-09 14:38:08 +0200

    dir hotfix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import Header from "./pages/Header";
-import Footer from "./pages/Footer";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
