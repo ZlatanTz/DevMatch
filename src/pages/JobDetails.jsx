0e380cee61e29a8f97a685c476f97c0b954a2490
Luka Vucinic <luka.vucinic1999@gmail.com>
2025-09-10 15:07:57 +0200

    Style Update

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -36,35 +36,32 @@ const JobDetails = () => {
   };
 
   return (
-    <div className="max-w-6xl mx-auto px-4 py-8 bg-white min-h-screen">
+    <div className="max-w-6xl mx-auto px-4 py-8 bg-white min-h-screen min-w-[320px]">
       <img
         src={company_img}
         alt={`${company} logo`}
         className="w-full h-64 sm:h-80 md:h-96 lg:h-[400px] object-contain object-center rounded-lg mb-6 shadow-md"
       />
 
-      <div className="flex flex-wrap justify-around bg-federal-blue text-white p-4 rounded-lg mb-8">
+      <div className="flex flex-col items-center space-y-2 sm:flex-row sm:flex-wrap sm:justify-around sm:space-y-0 p-4 rounded-lg mb-8 bg-federal-blue text-white">
         <p className="mx-2 font-medium">{company}</p>
         <p className="mx-2 font-medium">{title}</p>
         <p className="mx-2 font-medium">{location}</p>
-        <p className="mx-2 font-medium">
-          {min_salary}€-{max_salary}€
-        </p>
+        <p className="mx-2 font-medium">{seniority}</p>
         <p className="mx-2 font-medium">{employment_type}</p>
         <p className="mx-2 font-medium">{is_remote ? "Remote" : "On-site"}</p>
       </div>
 
       <div className="container mx-auto">
         <div className="flex flex-col md:flex-row gap-6 mb-8">
-          <div className="job-description md:w-7/10 p-6 rounded-lg shadow border border-gray-200">
-            <p className="mb-4 text-gray-700">Job description goes here. </p>
+          <div className="job-description lg:w-7/10 md:w-6/10 p-6 rounded-lg shadow border border-gray-200">
             <p className="font-semibold text-paynes-gray">The role entails:</p>
             <p className="mb-4 text-gray-700">{description}</p>
             <p className="font-semibold text-paynes-gray">Requiremnt:</p>
-            <div className="mt-auto pt-4 flex items-center justify-between text-sm">
-              <SkillList names={skillNames} max={3} />
+            <div className="mt-auto pt-4  pb-4 flex items-center justify-between text-sm">
+              <SkillList names={skillNames} max={skillNames.length} />
             </div>
-            .<p className="font-semibold text-paynes-gray">What we offer:</p>
+            <p className="font-semibold text-paynes-gray">What we offer:</p>
             <p className="mb-4 text-gray-700">
               Job description goes here.Job description goes here.Job description goes here.Job
               description goes here.Job description goes here.Job description goes here.Job
@@ -72,14 +69,25 @@ const JobDetails = () => {
               description goes here.
             </p>
           </div>
-          <div className="job-side-details flex-1 p-6 rounded-lg shadow border border-gray-200">
+          <div className="job-side-details flex-1 p-6 rounded-lg shadow border border-gray-200 md:self-start">
+            <div className="flex justify-start items-center mb-4">
+              <p className="text-paynes-gray font-medium">Status:</p>
+              <p className="text-gray-700 pl-1">{status === "open" ? "Open" : "Closed"}</p>
+            </div>
+
             <div className="flex justify-start items-center mb-4">
               <p className="text-paynes-gray font-medium">Date posted:</p>
               <p className="text-gray-700 pl-1">{formattedDate}</p>
             </div>
-            <div className="flex justify-start items-center">
+            <div className="flex justify-start items-center mb-4">
               <p className="text-paynes-gray font-medium">Location:</p>
-              <p className="text-gray-700 pl-1">Podgorica</p>
+              <p className="text-gray-700 pl-1">{location}</p>
+            </div>
+            <div className="flex justify-start items-center ">
+              <p className="text-paynes-gray font-medium">Salary:</p>
+              <p className="text-gray-700 pl-1">
+                {min_salary}€ - {max_salary}€
+              </p>
             </div>
           </div>
         </div>
@@ -215,7 +223,7 @@ const JobDetails = () => {
                 required
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:ring-2 focus:ring-emerald focus:border-transparent transition text-sm"
               >
-                <option value="">Choose a level</option>
+                <option value="">Seniority</option>
                 <option value="intern">Intern</option>
                 <option value="junior">Junior</option>
                 <option value="medior">Medior</option>
