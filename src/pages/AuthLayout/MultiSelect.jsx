540b58d27286fe6aad523bb040257ec4f1be8bae
Zlatan Tuzovic <tuzovic.zlatan@gmail.com>
2025-09-11 11:21:41 +0200

    fix: added dynamic skills from mock api

--- src/pages/AuthLayout/MultiSelect.jsx
+++ src/pages/AuthLayout/MultiSelect.jsx
@@ -1,15 +1,15 @@
+import { useSkills } from "@/hooks/useSkills";
 import { Controller } from "react-hook-form";
 import Select from "react-select";
 
 const MultiSelect = ({ name, control, label, error }) => {
-  const options = [
-    { value: 1, label: "React" },
-    { value: 2, label: "Vue" },
-    { value: 3, label: "Angular" },
-    { value: 4, label: "FastAPI" },
-    { value: 5, label: "Djnago" },
-  ];
+  const { skills } = useSkills();
+  const options = skills.map((skill) => ({
+    value: skill.id,
+    label: skill.name,
+  }));
 
+  console.log(options);
   return (
     <div className="mb-4">
       {label && <label className="block mb-1 font-semibold">{label}</label>}
