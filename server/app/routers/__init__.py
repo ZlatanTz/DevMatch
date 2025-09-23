95b0d1d6fa93e5ff500166901cdce5a7b5442599
Zlatan Tuzovic <tuzovic.zlatan@gmail.com>
2025-09-23 15:41:34 +0200

    local bug fix in init py routers

--- server/app/routers/__init__.py
+++ server/app/routers/__init__.py
@@ -1,5 +1,4 @@
 from .roles import router as roles_router
 from .users import router as users_router
 from .jobs import router as jobs_router
-
 __all__ = ["roles_router", "users_router", "jobs_router"]
