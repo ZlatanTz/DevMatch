1ca3c6a6e2a16560bde5f3c18809d5ac750c579b
Luka Vucinic <luka.vucinic1999@gmail.com>
2025-09-26 12:07:12 +0200

    Update SkillTag.jsx

--- client/src/components/SkillTag.jsx
+++ client/src/components/SkillTag.jsx
@@ -2,7 +2,7 @@ export default function SkillTag({ isSelected, onClick, children, clickable = fa
   return (
     <span
       className={`px-2 py-1 rounded text-xs cursor-pointer text-white ${
-        isSelected ? "bg-emerald" : "bg-paynes-gray"
+        isSelected ? "bg-emerald" : "bg-federal-blue"
       }`}
       onClick={clickable ? onClick : undefined}
     >
